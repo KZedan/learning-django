a7941725f9f348f72c4cfbc85418fa2a3810281d
Kareem <kareem335@yahoo.co.uk>
2019-09-17 21:16:34 +0100

    added model

---
+++ learning_logs/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text

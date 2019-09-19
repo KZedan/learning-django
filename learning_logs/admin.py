f1e37c55057d9fbeefc109cd8c41d0f01817ab76
Kareem <kareem335@yahoo.co.uk>
2019-09-19 16:50:22 +0100

    configured url, view, and layout for homepage

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Topic
+from .models import Topic, Entry
 
 admin.site.register(Topic)
+admin.site.register(Entry)

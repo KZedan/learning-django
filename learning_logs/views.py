29bbe66c117324d858b5e0dfaecf114d7209e368
Kareem <kareem335@yahoo.co.uk>
2019-09-20 11:55:23 +0100

    added topics url and links to logging book

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,6 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
-  """The home page for Learning Log."""
-  return render(request, 'learning_logs/index.html')
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
